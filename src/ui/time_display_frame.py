1cf85bee3480025c2d52e443fb713c0c2fc4bc30
Seth Weiss <seth@e1na.com>
2025-09-02 11:06:04 -0700

    Update TimeDisplayFrame label

--- src/ui/time_display_frame.py
+++ src/ui/time_display_frame.py
@@ -3,7 +3,6 @@ Frame to hold the time display label.
 """
 import tkinter as tk
 from tkinter import ttk
-from datetime import datetime
 
 
 class TimeDisplayFrame(tk.Frame):
@@ -11,12 +10,12 @@ class TimeDisplayFrame(tk.Frame):
         super().__init__(**kwargs)
 
         # Labels
-        self.lbl_time = ttk.Label(master=self, text=f'time.percent%')
+        self.lbl_time_percent = ttk.Label(master=self, text=f'time.percent%')
 
         self.set_layout()
 
     def set_layout(self):
-        self.lbl_time.grid(row=0, column=0, sticky='nsew', padx=5, pady=5)
+        self.lbl_time_percent.grid(row=0, column=0, sticky='nsew', padx=5, pady=5)
 
     def update_label_text(self, txt):
-        self.lbl_time.configure(text=txt)
+        self.lbl_time_percent.configure(text=f'{txt:.4f}%')
