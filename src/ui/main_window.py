a8f32ef085a49515774177e0fcc8dc8743117ab3
Seth Weiss <seth@e1na.com>
2025-09-03 11:14:58 -0700

    Add removable toolbar

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -5,11 +5,12 @@ import tkinter as tk
 from tkinter import ttk
 
 from src.ui.main_frame import MainFrame
+from src.ui.toolbar import Toolbar
 
 
 # ICON_FILE = ...
-STARTING_WINDOW_WIDTH = 600
-STARTING_WINDOW_HEIGHT = 175
+STARTING_WINDOW_WIDTH = 550
+STARTING_WINDOW_HEIGHT = 170
 
 
 class MainWindow(tk.Tk):
@@ -28,7 +29,29 @@ class MainWindow(tk.Tk):
         self.window_height = STARTING_WINDOW_HEIGHT
         self.set_window_geometry()
 
-        self.main_frame = MainFrame(master=self, shift_start=kwargs.get('shift_start'), shift_len=kwargs.get('shift_len'))
+        # Toolbar
+        self.toolbar = Toolbar(master=self)
+
+        # Expand button
+        self.btn_expand_toolbar = tk.Button(
+            master=self,
+            text='+',
+            width=0,
+            height=0,
+            compound='center',
+            padx=0,
+            pady=0,
+            borderwidth=0,
+            command=self.show_toolbar
+        )
+
+        # Main frame
+        self.main_frame = MainFrame(
+            master=self,
+            shift_start=kwargs.get('shift_start'),
+            shift_len=kwargs.get('shift_len'),
+            # background='black'
+        )
 
     def set_window_geometry(self):
         screen_width = self.winfo_screenwidth()
@@ -39,6 +62,10 @@ class MainWindow(tk.Tk):
         y = (screen_height / 2) - (height / 2)
         self.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
+    def show_toolbar(self):
+        self.config(menu=self.toolbar)
+
     def run(self):
-        self.main_frame.grid(row=0, column=0, sticky='nsew', padx=5, pady=5)
+        self.btn_expand_toolbar.grid(row=0, column=0, sticky='nw', padx=(0, 0), pady=(0, 0))
+        self.main_frame.grid(row=1, column=0, sticky='nsew', padx=(5, 5), pady=(0, 0))
         self.mainloop()
---
+++ src/ui/toolbar.py
@@ -0,0 +1,55 @@
+"""
+A toolbar menu widget for the main window.
+"""
+import tkinter as tk
+
+
+class Toolbar(tk.Menu):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Menus
+        self.menu_file = tk.Menu(self.master, tearoff=0)
+        self.menu_edit = tk.Menu(self.master, tearoff=0)
+        self.menu_view = tk.Menu(self.master, tearoff=0)
+        self.menu_help = tk.Menu(self.master, tearoff=0)
+
+        self.set_menus()
+
+    def set_file_menu(self):
+        self.add_cascade(label='File', menu=self.menu_file)
+        self.menu_file.add_command(label='Exit', command=self.quit)
+
+    def set_edit_menu(self):
+        self.add_cascade(label='Edit', menu=self.menu_edit)
+        self.menu_edit.add_command(
+            label='Nothing here yet',
+            command=lambda: print('No Edit command yet')
+        )
+
+    def hide_toolbar(self):
+        self.master.config(menu='')
+
+    def set_view_menu(self):
+        self.add_cascade(label='View', menu=self.menu_view)
+        self.menu_view.add_cascade(
+            label='Hide toolbar',
+            command=self.hide_toolbar
+        )
+
+    def set_help_menu(self):
+        self.add_cascade(label='Help', menu=self.menu_help)
+        self.menu_help.add_command(
+            label='About',
+            command=lambda: print('No About command yet'),
+        )
+        self.menu_help.add_command(
+            label='User Guide',
+            command=lambda: print('No User Guide command yet')
+        )
+
+    def set_menus(self):
+        self.set_file_menu()
+        self.set_edit_menu()
+        self.set_view_menu()
+        self.set_help_menu()
