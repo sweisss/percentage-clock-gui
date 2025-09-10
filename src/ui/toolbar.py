ebc0fd356dd2172441717ee9c5f912abb7bab5cc
Seth Weiss <seth@e1na.com>
2025-09-10 09:13:04 -0700

    Add about message

--- src/ui/toolbar.py
+++ src/ui/toolbar.py
@@ -2,8 +2,35 @@
 A toolbar menu widget for the main window.
 """
 import tkinter as tk
+from tkinter import messagebox
 
 from src.ui.edit_workday_popup import EditWorkdayWindow
+
+
+ABOUT_MSG = ('This app is written in python 3.12.3 under the MIT licence.\n'
+            '---------------------------------------------------------------------------\n'
+            '\n'
+            'MIT License\n'
+            '\n'
+            'Copyright (c) 2025 Seth Weiss\n'
+            '\n'
+            'Permission is hereby granted, free of charge, to any person obtaining a copy'
+            'of this software and associated documentation files (the "Software"), to deal'
+            'in the Software without restriction, including without limitation the rights'
+            'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
+            'copies of the Software, and to permit persons to whom the Software is'
+            'furnished to do so, subject to the following conditions:\n'
+            '\n'
+            'The above copyright notice and this permission notice shall be included in all'
+            'copies or substantial portions of the Software.\n'
+            '\n'
+            'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+            'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+            'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+            'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+            'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+            'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
+            'SOFTWARE.')
 
 
 class Toolbar(tk.Menu):
@@ -80,11 +107,17 @@ class Toolbar(tk.Menu):
         )
 
     ###### Help Menu ######
+    def display_about_message(self):
+        messagebox.showinfo(
+            title=self.main_window.title(),
+            message=ABOUT_MSG,
+        )
+
     def set_help_menu(self):
         self.add_cascade(label='Help', menu=self.menu_help)
         self.menu_help.add_command(
             label='About',
-            command=lambda: print('No About command yet'),
+            command=self.display_about_message,
         )
         self.menu_help.add_command(
             label='User Guide',
